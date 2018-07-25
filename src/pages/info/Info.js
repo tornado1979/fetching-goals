b12b6d56b67f50a8d5bd114e522341bfc748a9f4
Theodoros Itzaris <theodoreme@gmail.com>
2018-07-25 08:50:18 +0300

    Add Home & Info pages skeleton

---
+++ src/pages/info/Info.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Info extends Component {
+  render() {
+    return (
+      <div>
+        Info page
+      </div>
+    )
+  }
+}
+
+export default Info
