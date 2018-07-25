6e341d255fdc9941434d11c94ce43cd940998e2e
Theodoros Itzaris <theodoreme@gmail.com>
2018-07-25 11:59:52 +0300

    Change Home page so that dispatches REQUEST_GOALS action on componentDidMount and update state

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,6 +1,25 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import propTypes from 'prop-types'
+import queryString from 'query-string'
+
+import { fetchGoals } from './actionCreators/home.actionCreators'
 
 class Home extends Component {
+  componentDidMount() {
+    // destructure 'getGoals' from props
+    const {
+      getGoals,
+      location,
+    } = this.props
+
+    // get the queryString from url, as an object {'key': 'value'}
+    const parsed = queryString.parse(location.search)
+
+    // call 'getGoals' from actionCreators
+    getGoals(parsed)
+  }
+
   render() {
     return (
       <div>
@@ -10,4 +29,21 @@ class Home extends Component {
   }
 }
 
-export default Home
+
+Home.propTypes = {
+  getGoals: propTypes.func.isRequired,
+  location: propTypes.shape({
+    search: propTypes.string.isRequired,
+  }).isRequired,
+}
+
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    getGoals: (params) => {
+      dispatch(fetchGoals(params))
+    },
+  }
+}
+
+export default connect(null, mapDispatchToProps)(Home)
