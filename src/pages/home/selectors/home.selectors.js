9a4ee429ceb266a3e93acaac17238a94be3a38ab
Theodoros Itzaris <theodoreme@gmail.com>
2018-07-26 10:00:44 +0300

    Create selector 'getGoalById'

--- src/pages/home/selectors/home.selectors.js
+++ src/pages/home/selectors/home.selectors.js
@@ -6,3 +6,12 @@ export const getGoals = createSelector(
   getLocalState,
   goals => goals || {}, // the whole object {goals:[], isFetching: boolean}
 )
+
+export const getGoalById = createSelector(
+  (id) => id,
+  (id, state) => getGoals(state),
+  (id, goals) => {
+    const goal = goals.goals.filter(item => item.id === id) // filter goals array
+    return goal.length === 0 ? null : goal[0] // if goal does not exist i return null
+  },
+)
