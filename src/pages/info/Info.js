dd2c4c7ee0eadec85cc3304cd988daf9e11df2f5
Theodoros Itzaris <theodoreme@gmail.com>
2018-07-26 10:01:32 +0300

    Info page, searches on state and displays the selected goal

--- src/pages/info/Info.js
+++ src/pages/info/Info.js
@@ -1,13 +1,49 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
+
+import { GoalCard } from '../../components/goalCard'
+
+import * as selectors from '../home/selectors/home.selectors'
 
 class Info extends Component {
+  componentDidMount() {
+
+  }
+
   render() {
+    const goalInfo = this.props.goal// eslint-disable-line
+
+    if (!goalInfo) {
+      return (
+        <p>
+          Sorry , this item does not exist.
+          Please go back and chose another card
+          <Link to="/">
+            here
+          </Link>
+        </p>
+      )
+    }
+
+    // if item exists on state i display it
     return (
       <div>
-        Info page
+        <GoalCard
+          goal={goalInfo}
+          key="001"
+        />
       </div>
     )
   }
 }
 
-export default Info
+const mapStateToProps = (state, ownProps) => {
+  const goalId = ownProps.match.params.id
+
+  return {
+    goal: selectors.getGoalById(goalId, state),
+  }
+}
+
+export default connect(mapStateToProps, null)(Info)
