c2056eb801c338b4e320311f55a3e2a1067e421e
Theodoros Itzaris <theodoreme@gmail.com>
2018-07-26 14:09:18 +0300

    Separate dom structure for Home and for Info

--- src/components/goalCard.js
+++ src/components/goalCard.js
@@ -3,31 +3,98 @@ import propTypes from 'prop-types'
 import moment from 'moment'
 import Icon from '@material-ui/core/Icon'
 import { Link } from 'react-router-dom'
+import classnames from 'classnames'
 
 import { Progress } from './progress'
 import noImageAvailable from '../assets/img/No_Image_Available.jpg'
 import { CustomButton } from './customButton'
 
-export const GoalCard = ({ goal, idx }) => {
+export const GoalCard = ({
+  goal,
+  detailsStyle,
+}) => {
   const goalReleaseDate = moment(goal.date)
   const img = !goal.image_url ? noImageAvailable : goal.image_url
   const percentage = parseFloat(goal.percentage) * 100
   const to = `/goal/${goal.id}`
+  const comesFromDetailsPage = detailsStyle // if it comes from details page
+  const completed = goal.status === 'COMPLETED'
 
+  const IconStyle = {
+    fontSize: '15px',
+  }
+
+  const bodyStyle = classnames(
+    'goalBlock-body',
+    { details: detailsStyle },
+  )
   return (
-    <div className="goalBlock" key={idx}>
+    <div className="goalBlock">
       <div className="goalBlock-img">
         <div className="goalBlock-name">
           {goal.name}
         </div>
         <img alt={goal.name} src={img} />
       </div>
-      <div className="goalBlock-body">
+      {comesFromDetailsPage && (
+      <div className={bodyStyle}>
+        <div>
+          <Progress
+            percentage={percentage}
+          />
+        </div>
+        {completed
+          && (
+          <div className="goalBlock-button">
+            <Link to={to}>
+              <CustomButton
+                completed={completed}
+                link="/"
+                small={false}
+                text="Release Funds"
+              />
+            </Link>
+          </div>
+          )}
+        <div className="wrap-goalBlock-top">
+          <div>
+            <p className="goalBlock-title">
+              {goal.name}
+            </p>
+            <p className="goalBlock-description">
+              {goal.description}
+            </p>
+          </div>
+          <div>
+            <p className="goalBlock-balance">
+              {goal.balance}
+            </p>
+            <p className="goalBlock-balance amount">
+              {goal.amount}
+            </p>
+          </div>
+        </div>
+        <div className="view-more">
+          <p className="goalBlock-date">
+            {goal.date && `By ${goalReleaseDate.format('Do MMM YYYY')}`}
+            {!goal.date
+              && (
+              <Icon>
+                report_off
+              </Icon>
+              )}
+          </p>
+        </div>
+      </div>
+      )}
+      {!comesFromDetailsPage && (
+      <div className={bodyStyle}>
         <div className="goalBlock-button">
           <Link to={to}>
             <CustomButton
-              completed={percentage === 100}
-              link="/"
+              completed={completed}
+              small
+              text="..."
             />
           </Link>
         </div>
@@ -51,18 +118,20 @@ export const GoalCard = ({ goal, idx }) => {
             {goal.date && `By ${goalReleaseDate.format('Do MMM YYYY')}`}
             {!goal.date
               && (
-              <Icon>
+              <Icon style={IconStyle}>
                 report_off
               </Icon>
               )}
           </p>
         </div>
       </div>
+      )}
     </div>
   )
 }
 
 GoalCard.propTypes = {
+  detailsStyle: propTypes.bool,
   goal: propTypes.shape({
     amount: propTypes.number,
     balance: propTypes.number,
@@ -70,5 +139,8 @@ GoalCard.propTypes = {
     name: propTypes.string,
     percentage: propTypes.number,
   }).isRequired,
-  idx: propTypes.number.isRequired,
+}
+
+GoalCard.defaultProps = {
+  detailsStyle: false, // it come false from 'Home' and true from 'Info' pages
 }
