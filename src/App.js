5d144f8d035bf5727b7c24c8d52d65cd186bcb6e
Theodoros Itzaris <theodoreme@gmail.com>
2018-07-27 16:13:55 +0300

    Fixing the default unit test that was crashing. Moved Provider to App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react'
 import { BrowserRouter as Router } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import store from './store'
+
 import logo from './logo.svg'
 import './App.scss'
 
@@ -8,17 +11,19 @@ import { Routes } from './components/routes'
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img alt="logo" className="App-logo" src={logo} />
-          <h1 className="App-title">
-            React/redux app that fetches data from express server
-          </h1>
-        </header>
-        <Router>
-          <Routes />
-        </Router>
-      </div>
+      <Provider store={store}>
+        <div className="App">
+          <header className="App-header">
+            <img alt="logo" className="App-logo" src={logo} />
+            <h1 className="App-title">
+              React/redux app that fetches data from express server
+            </h1>
+          </header>
+          <Router>
+            <Routes />
+          </Router>
+        </div>
+      </Provider>
     )
   }
 }
